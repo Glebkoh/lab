f3d63a59a13c93bf6f417ff8016c798e4b571a5b
Glebkoh <glebkostarev2002@gmail.com>
2020-09-24 21:51:28 +0300

    Очистил файл

--- lab2/lab2re/Source.cpp
+++ lab2/lab2re/Source.cpp
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #define _CRT_SECURE_NO_WARNING
 #include <math.h>
+void Function_while(float x, float y, float i, float step);
+void Function_for(float x, float y, float i, float step);
+void Function_do_while(float x, float y, float i, float step);
 void Function(float x);
 int main()
 {
 	float x, y;
-	int N; // количество шагов
+	int N;
 	printf("Give me interval:\n");
 	scanf_s("%f%f", &x, &y);
 	printf("Give me number N:\n");
 	scanf_s("%d", &N);
 	float step = (y - x) / N;
+	float i = x;
+	Function_do_while(x, y, i, step);
+	Function_for(x, y, i, step);
+	Function_while(x, y, i, step);
+}
+void Function_do_while(float x, float y, float i, float step)
+{
+	do
+	{
+		Function(i);
+		i = i + step;
+	} while (i < y + 0.0001 && step != 0);
+}
+void Function_for(float x, float y, float i, float step)
+{
 	if (step != 0)
 	{
 		for (float i = x; i < y + 0.0001; i = i + step)
@@ -22,7 +40,14 @@ int main()
 	{
 		printf("Step = 0 ");
 	}
-	return 0;
+}
+void Function_while(float x, float y, float i, float step)
+{
+	while (i < y + 0.0001 && step != 0)
+	{
+		Function(i);
+		i = i + step;
+	}
 }
 void Function(float x)
 {
@@ -41,8 +66,9 @@ void Function(float x)
 	{
 		printf(" X = %6.3f ", x);
 		printf("|");
-		printf(" None       ");
+		printf(" None ");
 		printf("|\n");
 	}
 	printf("+------------+------------+\n");
+
 }
