ecb9ce25013ac0782c575afa127fbb5bcf2ad8d1
Glebkoh <glebkostarev2002@gmail.com>
2020-09-27 17:36:32 +0300

    Изменил лабу 2

--- lab2/project16/Source.cpp
+++ lab2/project16/Source.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#define _CRT_SECURE_NO_WARNING
 #include <math.h>
-void Function_while(float x, float y, float i, float step);
-void Function_for(float x, float y, float i, float step);
-void Function_do_while(float x, float y, float i, float step);
-void Function(float x);
+
+#define _CRT_SECURE_NO_WARNING
+
+void functionWhile(float x, float y, float i, float step);
+void functionFor(float x, float y, float i, float step);
+void functionDoWhile(float x, float y, float i, float step);
+void function(float x);
+
 int main()
 {
 	float x, y;
@@ -15,25 +18,35 @@ int main()
 	scanf_s("%d", &N);
 	float step = (y - x) / N;
 	float i = x;
-	Function_do_while(x, y, i, step);
-	Function_for(x, y, i, step);
-	Function_while(x, y, i, step);
+
+	functionDoWhile(x, y, i, step);
+
+	functionFor(x, y, i, step);
+
+	functionWhile(x, y, i, step);
+
 }
-void Function_do_while(float x, float y, float i, float step)
+
+void functionDoWhile(float x, float y, float i, float step)
 {
+
 	do
 	{
-		Function(i);
+
+		function(i);
 		i = i + step;
 	} while (i < y + 0.0001 && step != 0);
+
 }
-void Function_for(float x, float y, float i, float step)
+
+void functionFor(float x, float y, float i, float step)
 {
+
 	if (step != 0)
 	{
 		for (float i = x; i < y + 0.0001; i = i + step)
 		{
-			Function(i);
+			function(i);
 		}
 	}
 	else
@@ -41,15 +54,18 @@ void Function_for(float x, float y, float i, float step)
 		printf("Step = 0 ");
 	}
 }
-void Function_while(float x, float y, float i, float step)
+
+void functionWhile(float x, float y, float i, float step)
 {
+
 	while (i < y + 0.0001 && step != 0)
 	{
-		Function(i);
+		function(i);
 		i = i + step;
 	}
 }
-void Function(float x)
+
+void function(float x)
 {
 	float F;
 	printf("+------------+------------+\n");
