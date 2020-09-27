51e29d162c3e9b6ce6539a5a9a857c8e9347634a
Glebkoh <glebkostarev2002@gmail.com>
2020-09-27 18:26:49 +0300

    Добавил изменение в лабу 1

--- lab1/lab1.1/hello.c
+++ lab1/lab1.1/hello.c
@@ -1,11 +1,14 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int check(float x, float y);
+
+#define _CRT_SECURE_NO_WARNINGS
+
+int checkPointPosition(float x, float y);
+
 int main()
 {
 	float x, y;
 	scanf_s("%f%f", &x, &y);
-	int location = check(x, y);
+	int location = checkPointPosition(x, y);
 	if (location == 0)
 		printf("Point on the line");
 	else if (location > 0)
@@ -13,15 +16,16 @@ int main()
 	else if (location < 0)
 		printf("Point under the line");
 
+	
 }
-int check(float x, float y)
+int checkPointPosition(float x, float y)
 {
-	int i;
+	int result;
 	if (y - 1 / 2 * x - 1 == 0)
-		i = 0;
+		result = 0;
 	else if (y - 1 / 2 * x - 1 > 0)
-		i = 1;
+		result = 1;
 	else if (y - 1 / 2 * x - 1 < 0)
-		i = -1;
-	return i;
+		result = -1;
+	return result;
 }
--- lab1/lab1.2/labwork2.cpp
+++ lab1/lab1.2/labwork2.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
+
 #define _CRT_SECURE_NO_WARNING
+
+void function1DoesThePointLieOnLines(float x, float y);
+void function2IsThePointInTheRegion(float y1, float y2, float y3, float x, float y);
+void function3PointLiesOnAStraightLine(float y1, float y2, float y3, float y);
 
 int main()
 {
@@ -8,32 +13,49 @@ int main()
 
 	float y1 = 2 * x + 2;
 	float y2 = 1 / 2 * x - 1;
-	float y3 = 2 - x ;
-	if (x == 0 && y == 2)
-		printf("The point lies at the intersection of lines 1 and 3");
-	else if (x == 4 && y == 0)
-		printf("The point lies at the intersection of lines 2 and 3");
-	else if (x == -4 && y == -2)
-		printf("The point lies at the intersection of lines 1 and 2");
-	else if (y < y1 && y > y3 && y > y2)
-		printf(" region 1");
-	else if ( y < y2 && y > y3)
-		printf(" region 2");
-	else if (y < y1 && y < y2 && y < y3)
-		printf(" region 3");
-	else if (y < y2 && y > y1)
-		printf("region 4");
-	else if (y > y1 && y > y2 && y < y3)
-		printf("region 5");
-	else if (y > 2 && y > y1 && y > y3)
-		printf("region 6");
-	else if (y > y2 && y < y1 && y < y3)
-		printf("region 7");
-	else if (y == y1)
-		printf("1");
-	else if (y == y2)
-		printf("2");
-	else if (y == y3)
-		printf("3");
-	return 0;
+	float y3 = 2 - x;
+
+	function1DoesThePointLieOnLines(x, y);
+	function2IsThePointInTheRegion(y1, y2, y3, y, x);
+	function3PointLiesOnAStraightLine(y1, y2, y3, y);
 }
+
+	void function1DoesThePointLieOnLines(float x, float y)
+	{
+		if (x == 0 && y == 2)
+			printf("The point lies at the intersection of lines 1 and 3");
+		else if (x == 4 && y == 0)
+			printf("The point lies at the intersection of lines 2 and 3");
+		else if (x == -4 && y == -2)
+			printf("The point lies at the intersection of lines 1 and 2");
+
+	}
+
+	void function2IsThePointInTheRegion(float y1, float y2, float y3, float x, float y)
+	{
+		if (y < y1 && y > y3 && y > y2)
+			printf(" region 1");
+		else if (y < y2 && y > y3)
+			printf(" region 2");
+		else if (y < y1 && y < y2 && y < y3)
+			printf(" region 3");
+		else if (y < y2 && y > y1)
+			printf("region 4");
+		else if (y > y1 && y > y2 && y < y3)
+			printf("region 5");
+		else if (y > 2 && y > y1 && y > y3)
+			printf("region 6");
+		else if (y > y2 && y < y1 && y < y3)
+			printf("region 7");
+	}
+	void function3PointLiesOnAStraightLine(float y1, float y2, float y3, float y)
+	{
+		if (y == y1)
+			printf(" line 1");
+		else if (y == y2)
+			printf(" line 2");
+		else if (y == y3)
+			printf(" line 3");
+		getchar();
+		getchar();
+	}
