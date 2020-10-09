7ecb75db9c803c5c2f4c94ed21bde99c00ed020e
Gleb Kostarev <70997537+Glebkoh@users.noreply.github.com>
2020-10-09 14:37:57 +0300

    Update lab3.2.cpp

--- lab3/lab3.2/lab3.2.cpp
+++ lab3/lab3.2/lab3.2.cpp
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-void calculationIntegral(float S_past, float S_prev, float a, float b, float e, float h, float x_0, float N, float x_i, float x_1, float S_sum);
-void findingTheIntegral(float S_past, float S_prev, float a, float b, float e, float h, float x_0, float N, float x_i, float x_1, float S_sum);
+void calculateIntegral(float S_past, float S_prev, float a, float b, float e, float h, float x_0, float N, float x_i, float x_1, float S_sum);
+void searchTheIntegral(float S_past, float S_prev, float a, float b, float e, float h, float x_0, float N, float x_i, float x_1, float S_sum);
 
 int main()
 {
 	float S_past = 0, S_prev = 0, a = 0, b = 0, e = 0, h = 0, x_0 = 0, N = 0, x_1 = 0, x_i = 0, S_sum = 0;
 
-	calculationIntegral(S_past, S_prev, a, b, e, h, x_0, N, x_i, x_1, S_sum);
+	calculateIntegral(S_past, S_prev, a, b, e, h, x_0, N, x_i, x_1, S_sum);
 }
 
-void calculationIntegral(float S_past, float S_prev, float a, float b, float e, float h, float x_0, float N, float x_i, float x_1, float S_sum)
+void calculateIntegral(float S_past, float S_prev, float a, float b, float e, float h, float x_0, float N, float x_i, float x_1, float S_sum)
 {
 	printf(" Calculation accuracy : \n e = ");
 	scanf_s("%f", &e);
@@ -28,13 +28,13 @@ void calculationIntegral(float S_past, float S_prev, float a, float b, float e, 
 	S_past = h * log((x_0 + x_1) / 2);
 	while ((S_past - S_prev) > e)
 	{
-		findingTheIntegral(S_past, S_prev, a, b, e, h, x_0, N, x_i, x_1, S_sum);
+		searchTheIntegral(S_past, S_prev, a, b, e, h, x_0, N, x_i, x_1, S_sum);
 	}
 	printf(" S_sum = ");
 	printf("%f", S_sum);
 }
 
-void findingTheIntegral(float S_past, float S_prev, float a, float b, float e, float h, float x_0, float N, float x_i, float x_1, float S_sum)
+void searchTheIntegral(float S_past, float S_prev, float a, float b, float e, float h, float x_0, float N, float x_i, float x_1, float S_sum)
 {
 	S_prev = S_past;
 	N *= 2;
