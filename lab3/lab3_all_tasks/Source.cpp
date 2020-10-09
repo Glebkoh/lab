2b6f0cae0496fe1d6a4ae4452ec786902f9b986a
Gleb Kostarev <70997537+Glebkoh@users.noreply.github.com>
2020-10-09 14:35:03 +0300

    Update Source.cpp

--- lab3/lab3_all_tasks/Source.cpp
+++ lab3/lab3_all_tasks/Source.cpp
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-void function1(float S_last, float Sprev, float fact, float Sum, float E);
-void function2(float S_past, float S_prev, float a, float b, float e, float h, float x_0, float N, float x_i, float x_1, float S_sum);
+void calculateSumOfAnInfiniteSeries(float S_last, float Sprev, float fact, float Sum, float E);
+void calculateTheAmount(float S_last, float Sprev, float fact, float Sum, float E, int i);
 
 int main()
 {
-	float S_past = 0, S_prev = 0, a = 0, b = 0, e = 0, h = 0, x_0 = 0, N = 0, x_1 = 0, x_i = 0, S_sum = 0; 
-	float S_last = 0, Sprev = 0, fact = 1, Sum = 0, E = 0; 
+	float S_last = 0, Sprev = 0, fact = 1, Sum = 0, E = 0;
 
-	function1(S_last, Sprev, fact, Sum, E);
-
-	function2(S_past, S_prev, a, b, e, h, x_0, N, x_i, x_1, S_sum);
+	calculateSumOfAnInfiniteSeries(S_last, Sprev, fact, Sum, E);
 }
 
-void function1(float S_last, float Sprev, float fact, float Sum, float E)
+void calculateSumOfAnInfiniteSeries(float S_last, float Sprev, float fact, float Sum, float E)
 {
 	printf("First task\n");
 	int i = 1;
@@ -31,49 +28,25 @@ void function1(float S_last, float Sprev, float fact, float Sum, float E)
 	{
 		while (fabsf(S_last - Sprev) > E)
 		{
-			Sprev = S_last;
-			i++;
-			fact *= i;
-			S_last = 1 / fact;
-			Sum = Sum + S_last;
+			calculateTheAmount(S_last, Sprev, fact, Sum, E, i);
 		}
 		printf(" Sum of factorials : \n  Sum = ");
 		printf("%f", Sum);
 	}
 }
 
-void function2(float S_past, float S_prev, float a, float b, float e, float h, float x_0, float N, float x_i, float x_1, float S_sum)
+void calculateTheAmount(float S_last, float Sprev, float fact, float Sum, float E, int i)
 {
-	printf("\n-------------------------------------------------");
-	printf("\nSecond task\n");
-	printf(" Calculation accuracy : \n  e = ");
-	scanf_s("%f", &e);
-	printf("  N = ");
-	scanf_s("%f", &N);
-	printf(" Integration limits\n");
-	printf("  a = ");
-	scanf_s("%f", &a); 
-	printf("  b = ");
-	scanf_s("%f", &b);
-	h = (b - a) / N;
-	x_0 = a;
-	x_1 = a * h;
-	S_past = h * log((x_0 + x_1) / 2);
-	while ((S_past - S_prev) > e)
+	i = 0;
+	while (fabsf(S_last - Sprev) > E)
 	{
-		S_prev = S_past;
-		N *= 2;
-		S_prev = S_past;
-		S_sum = S_sum + S_past;
-		h = (b - a) / N;
-		x_1 = a * h;
-		x_i = a * ((b - a) / (2 * N));
-		S_past = h * log((x_1 + x_i) / 2);
+		Sprev = S_last;
+		i++;
+		fact *= i;
+		S_last = 1 / fact;
+		Sum = Sum + S_last;
 	}
-	printf("  S_sum = ");
-	printf("%f", S_sum);
 }
-
 
 
 
