28cbb95cd8e1d8aadfb59e52b7d1deb6029d6aae
Gleb Kostarev <70997537+Glebkoh@users.noreply.github.com>
2020-10-11 13:57:45 +0300

    Update Source.cpp

--- lab5/var3/Project21/Source.cpp
+++ lab5/var3/Project21/Source.cpp
@@ -3,58 +3,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void enterArrayElements(int* a, int i, int n, int j, int Swap);
-void changeFunction(int j, int* a, int Swap);
-void checkingVariables(int* a, int i, int n, int j, int Swap);
+void enterArrayElements(int* a, int i, int n, int j);
+void changeFunction(int j, int* a);
+void outputArrayElement(int i, int n, int* a);
+void checkVariables(int* a, int i, int n, int j);
 
 int main()
 {
     int* a;
-    int i = 0, n, j = 0, Swap = 0;
+    int i = 0, n, j = 0;
     printf(" Enter the size of the array: \n");
     scanf_s("%d", &n);
     a = (int*)malloc(n * sizeof(int)); // Âûäåëåíèå ïàìÿòè
 
-    enterArrayElements(a, i, n, j, Swap);
-
-    for (i = 0; i < n; i++)
-        printf("%d ", a[i]);
+    enterArrayElements(a, i, n, j);
+    outputArrayElement(i, n, a);
 
     free(a);
     return 0;
 }
 
-void enterArrayElements(int* a, int i, int n, int j, int Swap)
+void enterArrayElements(int* a, int i, int n, int j)
 {
     for (i = 0; i < n; i++) // Ââîä ýëåìåíòîâ ìàññèâà
     {
         printf("a[%d] = ", i);
         scanf_s("%d", &a[i]);
     }
-    checkingVariables(a, i, n, j, Swap);
+    checkVariables(a, i, n, j);
 }
 
-void checkingVariables(int* a, int i, int n, int j, int Swap)
+void checkVariables(int* a, int i, int n, int j)
 {
     for (i = 0; i < n; i++)  // ñðàâíèâàåì äâà ñîñåäíèõ ýëåìåíòà.
     {
         for (j = 0; j < n - i - 1; j++)
         {
-            if (a[j] < a[j + 1] ) // ïîëîæèòåëüíûå ïî óáûâàíèþ 
+            if (a[j] < a[j + 1]) // ïîëîæèòåëüíûå ïî óáûâàíèþ 
             {
-                changeFunction(j, a, Swap);
+                changeFunction(j, a);
             }
             else if (a[j] > a[j + 1] && a[j] < 0)
             {
-                changeFunction(j, a, Swap);
+                changeFunction(j, a);
             }
         }
     }
 }
 
-void changeFunction(int j, int* a, int Swap)
+void outputArrayElement(int i, int n, int *a)
 {
-    Swap = a[j];
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+}
+
+void changeFunction(int j, int* a)
+{
+    int swap = 0;
+    swap = a[j];
     a[j] = a[j + 1];
-    a[j + 1] = Swap;
+    a[j + 1] = swap;
 }
