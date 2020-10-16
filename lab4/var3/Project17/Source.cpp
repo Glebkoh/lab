27f09b92e03af81fc464d32ac49e9b764cdb6f5c
Gleb Kostarev <70997537+Glebkoh@users.noreply.github.com>
2020-10-16 21:08:52 +0300

    Update Source.cpp

--- lab4/var3/Project17/Source.cpp
+++ lab4/var3/Project17/Source.cpp
@@ -1,71 +1,72 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define _CRT_SECURE_NO_WARNINGS
 #define ARRAY_SIZE 10
 
-void theMinimumElementArray(int min, int index, int a[ARRAY_SIZE], int i);
-void findingNumberOfElements(int count, int a[ARRAY_SIZE], int i, int min);
-void calculatingArithmeticSum(int count, int a[ARRAY_SIZE], float S);
+
+int searchTheMinimumElementArray(int min, int index, int* a, int i);
+float findGeoSum(int* a, int i, int min);
+void enterArrayElements(int* a, int i);
 
 
 int main()
 {
-    int a[ARRAY_SIZE];
+    int* a;
     int min = 0;
-    int i = 0, index = 0, count = 0;
-    float S = 0;
+    int i = 0, index = 0;
+    float S = 0.0;
+    a = (int*)malloc(ARRAY_SIZE * sizeof(int));
 
-    for ( int i = 0; i < ARRAY_SIZE; i++) // ввод значений массиву                                                                                            1
-    {
-        printf(" a[%d] = ", i);
-        scanf_s("%d", &a[i]);
-    }
-    theMinimumElementArray(min, index, a, i);
+    enterArrayElements(a, i);
+
+    min = searchTheMinimumElementArray(min, index, a, i);
+    printf(" Minimum element of the array : \n min = ");
+    printf("%d", min);
+
+    S = findGeoSum(a, i, min);
+    printf("\n The sum of a geometric progression:");
+    printf("\n S = %0.3f", S);
 }
 
-
-
-void theMinimumElementArray(int min, int index, int a[ARRAY_SIZE], int i)
+void enterArrayElements(int* a, int i)
 {
-    int  count = 0;
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        printf("  a[%d] = ", i);
+        scanf_s("%d", &a[i]);
+    }
+}
+
+int searchTheMinimumElementArray(int min, int index, int* a, int i)
+{
+
     min = a[ARRAY_SIZE - 1];
     index = 0;
-    for (int i = 0; i < ARRAY_SIZE; i++) // нахождение минимального элемента массива                                                                          2
+    for (int i = 0; i < ARRAY_SIZE; i++)
     {
         while ((a[i]) < min)
         {
             min = a[i];
             index = i;
         }
-
     }
-    printf(" Minimum element of the array : \n min = ");
-    printf("%d", min);
-
-    findingNumberOfElements(count, a, i, min);
+    return min;
 }
 
-
-void findingNumberOfElements(int count, int a[ARRAY_SIZE], int i, int min)
+float findGeoSum(int* a, int i, int min)
 {
+    float power = 1;
+    float compositionArray = 1;
     float S = 0.0;
-    count = 0, i = 0;
-    while (a[i] != min && a[i] != 0 && i < 10) // нахождение чисел из массива удовлетвор€ющих условию                                                              3
+    i = 0;
+    while (a[i] != min && a[i] != 0 && i < 10)
     {
+        compositionArray *= a[i];
         i++;
-        count++;
-
+        power = 1.0 / i;
     }
-    printf(" \nThe number of array elements that are at a minimum : \n count = ");
-    printf("%d", count);
-
-    calculatingArithmeticSum(count, a, S);
-}
-
-
-void  calculatingArithmeticSum(int count, int a[ARRAY_SIZE], float S)
-{
-    S = ((a[0] + a[count])/2.0)* count; //  cумма арифметической прогресии 
-    printf("\n%f", S);
+    S = pow(compositionArray, power);
+    return S;
 }
